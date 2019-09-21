152d15956a6b6aa2d016fb848531f845197f3c0e
hanoli thom <hanoli_tl86@myhanoli.com>
2019-09-21 09:11:31 -0500

    first commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,26 @@
+/* Populate tables */
+INSERT INTO clientes (nombre, apellido, email, create_at, foto) VALUES('Hansel', 'Thomas', 'hanoli@gmail.com', '2019-09-19', '');
+INSERT INTO clientes (nombre, apellido, email, create_at, foto) VALUES('Elizabeth', 'Martinez', 'lizi855@gmail.com', '2019-09-19', '');
+
+/* Populate tabla productos */
+INSERT INTO productos (nombre, precio, create_at) VALUES('USB 64GB', 259990, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Samsung camara VX3456', 123490, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('DELL Notebook Z110', 37990, NOW());
+
+/* Creamos algunas facturas */
+INSERT INTO facturas (descripcion, observacion, cliente_id, create_at) VALUES('Factura equipos de oficina', null, 1, NOW());
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(1, 1, 1);
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(2, 1, 4);
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(1, 1, 5);
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(1, 1, 7);
+
+INSERT INTO facturas (descripcion, observacion, cliente_id, create_at) VALUES('Factura Bicicleta', 'Alguna nota importante!', 1, NOW());
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(3, 2, 6);
+
+/* Creamos algunos usuarios con sus roles */
+INSERT INTO `users` (username, password, enabled) VALUES ('cesar','$2a$10$O9wxmH/AeyZZzIS09Wp8YOEMvFnbRVJ8B4dmAMVSGloR62lj.yqXG',1);
+INSERT INTO `users` (username, password, enabled) VALUES ('hansel','$2a$10$DOMDxjYyfZ/e7RcBfUpzqeaCs8pLgcizuiQWXPkU35nOhZlFcE9MS',1);
+
+INSERT INTO `authorities` (user_id, authority) VALUES (1,'ROLE_USER');
+INSERT INTO `authorities` (user_id, authority) VALUES (2,'ROLE_ADMIN');
+INSERT INTO `authorities` (user_id, authority) VALUES (2,'ROLE_USER');
