40a38977bb688e7bb47614541575b76d7b585793
hanoli thom <hanoli_tl86@myhanoli.com>
2019-09-21 23:56:40 -0500

    Se modificaron los roles

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -25,3 +25,4 @@ INSERT INTO `users` (username, password, enabled) VALUES ('eliza','$2a$10$DOMDxj
 INSERT INTO `authorities` (user_id, authority) VALUES (1,'ROLE_USER');
 INSERT INTO `authorities` (user_id, authority) VALUES (2,'ROLE_ADMIN');
 INSERT INTO `authorities` (user_id, authority) VALUES (2,'ROLE_USER');
+INSERT INTO `authorities` (user_id, authority) VALUES (3,'ROLE_USER');
