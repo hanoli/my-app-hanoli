df0134c5af1bba8046c74da170b2d6990d4c37c7
hanoli thom <hanoli_tl86@myhanoli.com>
2019-09-22 23:04:41 -0500

    Se agrego controlador para ver lista de gastos

--- src/main/java/com/myhanoli/springboot/app/controllers/CuentaController.java
+++ src/main/java/com/myhanoli/springboot/app/controllers/CuentaController.java
@@ -2,18 +2,30 @@ package com.myhanoli.springboot.app.controllers;
 
 import java.io.IOException;
 import java.security.Principal;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestWrapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,21 +37,18 @@ import com.myhanoli.springboot.app.models.entity.Cliente;
 import com.myhanoli.springboot.app.models.entity.Gasto;
 import com.myhanoli.springboot.app.models.service.IClienteService;
 import com.myhanoli.springboot.app.models.service.IGastoService;
+import com.myhanoli.springboot.app.util.paginator.PageRender;
 
 @Controller
 public class CuentaController {
 
+	protected final Log logger = LogFactory.getLog(this.getClass());
+	
 	@Autowired
     private MessageSource messageSource;
 	
 	@Autowired
 	private IGastoService gastoService;
-	
-	@GetMapping("/cuenta")
-	public String cuenta(
-			Model model ) {
-		return "cuenta";
-	}
 
 
 	@Secured("ROLE_ADMIN")
@@ -62,14 +71,70 @@ public class CuentaController {
 			model.addAttribute("titulo", messageSource.getMessage("text.gasto.form.titulo.crear", null, locale));
 			return "altaCuenta";
 		}
-		System.out.println("Antes de mensajeFlash");
+
 		String mensajeFlash = (gasto.getId() != null) ? messageSource.getMessage("text.gasto.flash.editar.success", null, locale) : messageSource.getMessage("text.gasto.flash.crear.success", null, locale);
-System.out.println("Despues de mensajeFlash");
+
 		gastoService.save(gasto);
 		status.setComplete();
 		flash.addFlashAttribute("success", mensajeFlash);
 		return "redirect:cuenta";
 	}
+	
+	
+	@PreAuthorize("hasRole('ROLE_USER')")
+	@GetMapping(value = "/verGasto/{id}")
+	public String verGasto(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash, Locale locale) {
+      System.out.println("llegue al controlador ver");
+		List<Gasto> gasto = gastoService.findAll();
+		if (gasto.size() > 0) {
+			flash.addFlashAttribute("error", messageSource.getMessage("text.cliente.flash.db.error", null, locale));
+			return "redirect:/cuenta";
+		}
+
+		model.put("gasto", gasto);
+		model.put("titulo", messageSource.getMessage("text.cliente.detalle.titulo", null, locale).concat(": ").concat(gasto.get(0).getNombre()));
+		return "ver";
+	}
+	
+	
+	@RequestMapping(value = {"/cuenta"}, method = RequestMethod.GET)
+	public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model,
+			HttpServletRequest request,
+			Locale locale) {
+				
+		Pageable pageRequest = PageRequest.of(page, 4);
+
+		Page<Gasto> gastos = gastoService.findAll(pageRequest);
+		PageRender<Gasto> pageRender = new PageRender<Gasto>("/cuenta", gastos);
+		model.addAttribute("titulo", messageSource.getMessage("text.cliente.listar.titulo", null, locale));
+		model.addAttribute("gastos", gastos);
+		model.addAttribute("page", pageRender);
+		return "cuenta";
+	}
+
+
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@RequestMapping(value = "/altaCuenta/{id}")
+	public String editarGasto(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash, Locale locale) {
+
+		Gasto gasto = null;
+
+		if (id > 0) {
+			
+			gasto = gastoService.findOne(id);
+			if (gasto == null) {
+				flash.addFlashAttribute("error", messageSource.getMessage("text.cliente.flash.db.error", null, locale));
+				return "redirect:/cuenta";
+			}
+		} else {
+			flash.addFlashAttribute("error", messageSource.getMessage("text.cliente.flash.id.error", null, locale));
+			return "redirect:/cuenta";
+		}
+		model.put("gasto", gasto);
+		model.put("titulo", messageSource.getMessage("text.cliente.form.titulo.editar", null, locale));
+		return "altaCuenta";
+	}
+	
 	
 	
 }
